e573744ebb3309234c26b498f6e070df81412a88
enrique7mc <enrique7mc@gmail.com>
2016-04-24 22:36:02 -0500

    Convert Snack to DragSource high order component

--- source/Snack.js
+++ source/Snack.js
@@ -1,11 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { DragSource } from 'react-dnd';
+
+const snackSpec = {
+  beginDrag (props) {
+    return {
+      name: props.name
+    };
+  },
+  endDrag (props, monitor) {
+    const dragItem = monitor.getItem();
+    const dropResult = monitor.getDropResult();
+
+    if (dropResult) {
+      console.log(`You dropped ${dragItem.name} into ${dropResult.name}`);
+    }
+  }
+};
+
+let collect = (connect, monitor) => {
+  return {
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging()
+  };
+}
 
 class Snack extends Component {
   render () {
-    return (
-      <div>{ this.props.name}</div>
+    const { name, isDragging, connectDragSource } = this.props;
+    const opacity = isDragging ? 0.4 : 1;
+
+    const style = {
+      opacity: opacity
+    };
+
+    return connectDragSource(
+      <div className='snack' style={ style }>
+        { name }
+      </div>
     );
   }
 }
 
-export default Snack;
+Snack.propTypes = {
+  connectDragSource: PropTypes.func.isRequired,
+  isDragging: PropTypes.bool.isRequired,
+  name: PropTypes.string.isRequired
+};
+
+export default DragSource('snack', snackSpec, collect)(Snack);
